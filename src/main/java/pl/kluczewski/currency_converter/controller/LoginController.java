0a198e72280e910158c662bb366195030772a486
Dawid <notedeagle@gmail.com>
2021-03-21 22:41:27 +0100

    Added LoginControllerTest, and fixed CurrencyControllerTest

--- src/main/java/pl/kluczewski/currency_converter/controller/LoginController.java
+++ src/main/java/pl/kluczewski/currency_converter/controller/LoginController.java
@@ -9,4 +9,9 @@ public class LoginController {
 
     @PostMapping("/login")
     public void login(@RequestBody LoginCredentials credentials) {}
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "secured";
+    }
 }
--- src/test/java/pl/kluczewski/currency_converter/controller/CurrencyControllerTest.java
+++ src/test/java/pl/kluczewski/currency_converter/controller/CurrencyControllerTest.java
@@ -6,15 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest
+@WithMockUser
 class CurrencyControllerTest {
 
     @Autowired
