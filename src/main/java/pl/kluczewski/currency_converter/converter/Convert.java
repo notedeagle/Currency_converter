68e48f7a073812d20efa982a140bb5a604bd93eb
Dawid <notedeagle@gmail.com>
2021-03-22 09:50:30 +0100

    Fixed currency convert

--- src/main/java/pl/kluczewski/currency_converter/converter/Convert.java
+++ src/main/java/pl/kluczewski/currency_converter/converter/Convert.java
@@ -2,10 +2,10 @@ package pl.kluczewski.currency_converter.converter;
 
 public class Convert {
     public double fromPln(double quantity, double bid) {
-        return Math.round(quantity * bid * 100.0) / 100.0;
+        return Math.round(quantity / bid * 100.0) / 100.0;
     }
 
     public double toPln(double quantity, double bid) {
-        return Math.round(quantity / bid * 100.0) / 100.0;
+        return Math.round(quantity * bid * 100.0) / 100.0;
     }
 }
